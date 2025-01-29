7a3cbb347616cb53c41f996a80ef32b1ffe062b8
SyndromeMX <pokharkov.v.v@inbox.ru>
2025-01-29 13:45:37 +0300

    Полное преобразование в шаблонный вид, подготовка глобальной папки static

--- Teach_calc/Teach_calc/urls.py
+++ Teach_calc/Teach_calc/urls.py
@@ -21,5 +21,5 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
-    path('login/', views.login, name = "login"),
+    path('about/', views.about, name = "about"),
 ]
--- Teach_calc/main/views.py
+++ Teach_calc/main/views.py
@@ -4,8 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,"main/index.html")
+    context = {
+        "title" : "Главная страница",
+        "content" : "Это мой первый веб сайт на фремворке Django. До этого я писал код на чистом html. Приложение представляет из себя базу для упрощения работы репетиторов и подсчета заработка и часов, а также расписание и часы работы"
+        }
+
+    return render(request,"main/index.html", context)
     
-def login(request):
-    return render(request, "main/login.html")
+def about(request):
+    context = {
+        "title" : "О нас",
+        "content" : "Меня зовут Владислав я хочу стать хорошим программистом."
+        }
+    return render(request, "main/about.html", context)
 
