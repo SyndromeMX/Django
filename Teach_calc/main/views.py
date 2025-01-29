9a820a6572842ee63764b3ecbaef6ee1b34f8b72
SyndromeMX <pokharkov.v.v@inbox.ru>
2025-01-29 13:08:54 +0300

    Начало использования шаблонов

--- Teach_calc/main/views.py
+++ Teach_calc/main/views.py
@@ -4,9 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    context = {
-        "titile" : "Главная",
-        "content" : "Главная страница"
-    }
-    return render(request,"main/index.html", context)
+    return render(request,"main/index.html")
+    
+def login(request):
+    return render(request, "main/login.html")
 
